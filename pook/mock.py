b927054952be0353a5e929cb479670776d8ee0bc
Tomás Aparicio <tomas@aparicio.me>
2016-12-05 12:21:30 +0000

    fix(mock): add calls property

--- pook/mock.py
+++ pook/mock.py
@@ -655,6 +655,16 @@ class Mock(object):
             callback(request, self)
 
         return True
+    
+    @property
+    def calls(self):
+        """
+        Accessor to retrieve the number of mock matched calls.
+        
+        Returns:
+            int
+        """
+        return self._matches
 
     def __call__(self, fn):
         """
