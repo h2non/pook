4d9c0c62471aa27712c5c34d3ff01c24cdc81c5b
Tomás <tomas@aparicio.me>
2018-01-12 11:41:02 +0100

    fix(mock_engine): remote trailing spaces

--- pook/mock_engine.py
+++ pook/mock_engine.py
@@ -4,17 +4,17 @@ from .interceptors import interceptors
 class MockEngine(object):
     """
     ``MockEngine`` represents the low-level mocking engine abstraction
-    layer between ``pook`` and the underlying mocking mechanism 
+    layer between ``pook`` and the underlying mocking mechanism
     responsible of intercepting and trigger outgoing HTTP traffic within
     the Python runtime.
-    
+
     ``MockEngine`` implements the built-in `pook` mock engine based on HTTP
     interceptors strategy.
-    
+
     Developers can implement and plug in their own ``MockEngine`` in order
     to fit custom mocking logic needs.
-    
-    You can see a custom ``MockEngine`` implementation here: 
+
+    You can see a custom ``MockEngine`` implementation here:
     http://bit.ly/2EymMro
 
     Custom mock engines must implementent at least the following methods:
