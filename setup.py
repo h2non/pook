9bf46cceea3c55e9620df86a2594cd4e1d2b9ab7
Tomas <tomas@aparicio.me>
2019-11-01 22:09:29 +0100

    fix: indetntation format setup.py

--- setup.py
+++ setup.py
@@ -97,9 +97,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-	'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-	'Topic :: Software Development',
+        'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
