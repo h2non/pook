bfbad50e389bbe054f400e7ec1969dd6fee920be
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2023-12-23 09:59:29 +1100

    Reproduce bug in tests

--- tests/unit/matchers/headers_test.py
+++ tests/unit/matchers/headers_test.py
@@ -95,3 +95,74 @@ def test_headers_matcher(expected, requested, should_match):
 
     matched, explanation = mock.match(request)
     assert matched == should_match, explanation
+
+
+@pytest.mark.parametrize(
+    ("required_headers", "requested_headers", "should_match"),
+    (
+        pytest.param(
+            ["content-type", "Authorization"],
+            {
+                "Content-Type": "",
+                "authorization": "Bearer NOT A TOKEN",
+            },
+            True,
+            id="case-insensitive-match-with-empty-value"
+        ),
+        pytest.param(
+            ["content-type", "Authorization"],
+            {
+                "Content-Type": "application/json",
+                "authorization": "Bearer NOT A TOKEN",
+            },
+            True,
+            id="case-insensitive-match-with-non-empty-values"
+        ),
+        pytest.param(
+            ["x-requested-with"],
+            {
+                "content-type": "application/json",
+            },
+            False,
+            id="x-header-missing-with-other-headers"
+        ),
+        pytest.param(
+            ["x-requested-with"],
+            {},
+            False,
+            id="x-header-no-headers",
+        ),
+        pytest.param(
+            ["content-type"],
+            {},
+            False,
+            id="no-headers",
+        ),
+        pytest.param(
+            ["x-requested-with"],
+            {"x-requested-with": "com.example.app"},
+            True,
+            id="x-header-with-value"
+        ),
+        pytest.param(
+            ["x-requested-with"],
+            {"x-requested-with": ""},
+            True,
+            id="x-header-with-empty-value"
+        ),
+    )
+)
+def test_headers_present(required_headers, requested_headers, should_match):
+    mock = pook.get('https://example.com').headers_present(required_headers)
+
+    request = pook.Request()
+    request.url = 'https://example.com'
+    request.headers = requested_headers
+
+    matched, explanation = mock.match(request)
+    assert matched == should_match, explanation
+
+
+def test_headers_present_empty_headers():
+    with pytest.raises(ValueError):
+        pook.get('https://example.com').headers_present([])
