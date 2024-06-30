6cbb27c392473d5d3c81e3d053d9b72425436a4a
Wim Jeantine-Glenn <hey@wimglenn.com>
2024-06-29 22:02:28 -0500

    use context-manager

--- src/pook/pytest_fixture.py
+++ src/pook/pytest_fixture.py
@@ -6,6 +6,5 @@ import pook as pook_mod
 @pytest.fixture
 def pook():
     """Pytest fixture for HTTP traffic mocking and testing"""
-    pook_mod.on()
-    yield pook_mod
-    pook_mod.off()
+    with pook_mod.use():
+        yield pook_mod
