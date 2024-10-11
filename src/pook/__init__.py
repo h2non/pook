12739e1e40ffe49ca26b30874960be736d7aa786
sarayourfriend <git@sarayourfriend.pictures>
2024-10-11 15:57:24 +1100

    Use explicit __all__ for main module export

--- src/pook/__init__.py
+++ src/pook/__init__.py
@@ -1,8 +1,43 @@
-from .api import *  # noqa
-from .api import __all__ as api_exports
+from .api import *  # noqa: F403
 
 # Delegate to API export
-__all__ = api_exports
+__all__ = (
+    "activate",  # noqa: F405
+    "on",  # noqa: F405
+    "disable",  # noqa: F405
+    "off",  # noqa: F405
+    "reset",  # noqa: F405
+    "engine",  # noqa: F405
+    "use_network",  # noqa: F405
+    "enable_network",  # noqa: F405
+    "disable_network",  # noqa: F405
+    "get",  # noqa: F405
+    "post",  # noqa: F405
+    "put",  # noqa: F405
+    "patch",  # noqa: F405
+    "head",  # noqa: F405
+    "use",  # noqa: F405
+    "set_mock_engine",  # noqa: F405
+    "delete",  # noqa: F405
+    "options",  # noqa: F405
+    "pending",  # noqa: F405
+    "ispending",  # noqa: F405
+    "mock",  # noqa: F405
+    "pending_mocks",  # noqa: F405
+    "unmatched_requests",  # noqa: F405
+    "isunmatched",  # noqa: F405
+    "unmatched",  # noqa: F405
+    "isactive",  # noqa: F405
+    "isdone",  # noqa: F405
+    "regex",  # noqa: F405
+    "Engine",  # noqa: F405
+    "Mock",  # noqa: F405
+    "Request",  # noqa: F405
+    "Response",  # noqa: F405
+    "MatcherEngine",  # noqa: F405
+    "MockEngine",  # noqa: F405
+    "use_network_filter",  # noqa: F405
+)
 
 # Package metadata
 __author__ = "Tomas Aparicio"
